ee0ec81b555fd3692566967ecaee864198b5a181
vfridell <vfridell@bellarmine.edu>
2022-10-11 13:24:20 -0400

    switch to decimal

--- Logistic/Program.cs
+++ Logistic/Program.cs
@@ -8,26 +8,28 @@ namespace Logistic
     {
         static void Main(string[] args)
         {
-			double LogisticFunc(double x, double r) => r * x * (1 - x);
+			decimal LogisticFunc(decimal x, decimal r) => r * x * (1 - x);
 
 
 			int iterations = 1024;
 			int iter_min = 64;
 			int iter_max = iterations;
-			double match_tolerance = 1e-5;
+			decimal match_tolerance = 1e-5m;
 			Random rand = new Random();
-			double x = rand.NextDouble();
-			x = 0.4042476770372687;
+			decimal x = (decimal)rand.NextDouble();
+			//x = 0.4042476770372687;
 			Console.WriteLine($"x: {x}");
-			double r = 3.54409;
-			r = 1 + Math.Sqrt(8);
+			decimal r = 3.54409m;
+			//r = 1 + Math.Sqrt(8);
 			//r = 3.5693;
 			//r = 3.574;
 			//r = 3.82843;
-			//r = 3.543;
-			Console.WriteLine($"r: {r}");
+			r = 3.543m;
+			r = 3.56440726m;
 
-			List<double> values = new List<double>();
+            Console.WriteLine($"r: {r}");
+
+			List<decimal> values = new List<decimal>();
 			bool stop = false;
 			do
 			{
@@ -39,7 +41,7 @@ namespace Logistic
 			// reverse and trim the list
 			values = values.TakeLast(64).Reverse().ToList();
 			
-			List<double> OscillationValues = new List<double>();
+			List<decimal> OscillationValues = new List<decimal>();
 			for (int i = 0; i < values.Count; i++)
 			{
 				if (OscillationValues.Any(ov => NearlyEqual(values[i], ov, match_tolerance))) break;
@@ -53,19 +55,19 @@ namespace Logistic
 				}
 			}
 
-			Console.WriteLine("Oscillation Values");
+			Console.WriteLine($"Oscillation Values ({OscillationValues.Count})");
 			foreach (var v in OscillationValues.OrderByDescending(ov => ov))
 			{
 				Console.WriteLine(v);
 			}
 		}
 
-		static bool NearlyEqual(double a, double b, double epsilon)
+		static bool NearlyEqual(decimal a, decimal b, decimal epsilon)
 		{
-			const double MinNormal = 2.2250738585072014E-308d;
-			double absA = Math.Abs(a);
-			double absB = Math.Abs(b);
-			double diff = Math.Abs(b - a);
+			const decimal MinNormal = 2.2250738585072014E-308m;
+			decimal absA = Math.Abs(a);
+			decimal absB = Math.Abs(b);
+			decimal diff = Math.Abs(b - a);
 
 			if (a.Equals(b))
 			{ // shortcut, handles infinities
